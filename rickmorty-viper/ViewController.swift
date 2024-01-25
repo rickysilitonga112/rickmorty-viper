67d9b24f329696bb2b3f5bd446ac384d02ec573a
rickysilitonga112 <rickysilitonga112@gmail.com>
2024-01-25 17:39:06 +0700

    Initial Commit

---
+++ rickmorty-viper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rickmorty-viper
+//
+//  Created by Ricky Silitonga on 25/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
